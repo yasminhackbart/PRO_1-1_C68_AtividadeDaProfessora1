96892133769f73077c2a8247563561db7120827e
Thiago Saraiva <saraii_@live.com>
2022-05-04 10:28:10 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import BottomTabNavigator from "./components/BottomTabNavigator";
 
 export default class App extends Component {
   render() {
-    //adicione a instrução de retorno para BottomTabNavigator
-    
+    return <BottomTabNavigator />;
   }
 }
