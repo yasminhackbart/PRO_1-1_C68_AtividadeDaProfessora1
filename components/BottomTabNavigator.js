53e40e01e42195ea9520e034f9810b4fa95a7a01
Thiago Saraiva <saraii_@live.com>
2022-05-09 16:29:26 -0300

    Add files via upload

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import TransactionScreen from "../screens/Transaction";
 import SearchScreen from "../screens/Search";
@@ -10,12 +11,14 @@ const Tab = createBottomTabNavigator();
 export default class BottomTabNavigator extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Transação" component={TransactionScreen} />
-          <Tab.Screen name="Pesquisa" component={SearchScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Transação" component={TransactionScreen} />
+            <Tab.Screen name="Pesquisa" component={SearchScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     );
   }
 }
