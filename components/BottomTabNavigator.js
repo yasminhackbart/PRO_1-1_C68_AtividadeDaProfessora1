7ba68de2a53ee47d13a0c56cfff4c49d8261e6ad
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2023-06-29 17:37:22 -0300

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -3,21 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import TransactionScreen from "../screens/Transaction";
-import SearchScreen from "../screens/Search";
-
-const Tab = createBottomTabNavigator();
 
 export default class BottomTabNavigator extends Component {
   render() {
     return (
       <SafeAreaProvider>
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Transação" component={TransactionScreen} />
-            <Tab.Screen name="Pesquisa" component={SearchScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
+     
       </SafeAreaProvider>
     );
   }
